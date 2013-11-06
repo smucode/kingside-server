bc1ebb14fc9a16366d4e09cbeeb642f763f6ff2f
Stig Murberg <stig.murberg@gmail.com>
2013-11-06 21:06:27 +0100

    initial commit

---
+++ Gruntfile.js
@@ -0,0 +1,66 @@
+'use strict';
+
+var request = require('request');
+
+module.exports = function (grunt) {
+  require('time-grunt')(grunt); // show elapsed time at the end
+
+  require('load-grunt-tasks')(grunt); // load all grunt tasks
+
+  var reloadPort = 35729, files;
+
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    develop: {
+      server: {
+        file: 'app.js'
+      }
+    },
+    watch: {
+      options: {
+        nospawn: true,
+        livereload: reloadPort
+      },
+      server: {
+        files: [
+          'app.js',
+          'dist/*'
+        ],
+        tasks: ['develop', 'delayed-livereload']
+      },
+      js: {
+        files: ['dist/scripts/*.js'],
+        options: {
+          livereload: reloadPort
+        }
+      },
+      css: {
+        files: ['dist/styles/*.css'],
+        options: {
+          livereload: reloadPort
+        }
+      }
+    }
+  });
+
+  grunt.config.requires('watch.server.files');
+  files = grunt.config('watch.server.files');
+  files = grunt.file.expand(files);
+
+  grunt.registerTask('delayed-livereload', 'Live reload after the node server has restarted.', function () {
+    var done = this.async();
+    setTimeout(function () {
+      request.get('http://localhost:' + reloadPort + '/changed?files=' + files.join(','),  function (err, res) {
+          var reloaded = !err && res.statusCode === 200;
+          if (reloaded) {
+            grunt.log.ok('Delayed live reload successful.');
+          } else {
+            grunt.log.error('Unable to make a delayed live reload.');
+          }
+          done(reloaded);
+        });
+    }, 500);
+  });
+
+  grunt.registerTask('default', ['develop', 'watch']);
+};
